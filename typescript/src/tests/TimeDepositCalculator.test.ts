f91cb9c266b646ef65dc7f5cd6c8ec9f6215655b
Matt Cheung <matt.cheung@ikigaidigital.io>
2025-02-06 16:41:51 +0000

    update typescript test to match with other languages

--- typescript/src/tests/TimeDepositCalculator.test.ts
+++ typescript/src/tests/TimeDepositCalculator.test.ts
@@ -1,24 +1,11 @@
 import { TimeDeposit } from '../TimeDeposit'
 import { TimeDepositCalculator } from '../TimeDepositCalculator'
 
-const TEST_DATA = [
-  {
-    planType: 'basic',
-    balance: 1234567.0,
-    days: 45,
-    result: 1235595.81,
-  },
-]
-
-const plans: TimeDeposit[] = TEST_DATA.map(
-  (data) => new TimeDeposit(data.planType, data.balance, data.days),
-)
 
 test('Should correctly calculate interest', () => {
+  const plans: TimeDeposit[] = [new TimeDeposit('basic', 1234567.0, 45)]
   const calc = new TimeDepositCalculator()
-  const plansWithInterest = calc.calculateInterest(plans)
+  calc.calculateInterest(plans)
 
-  plansWithInterest.forEach((item, index) => {
-    expect(item.balance).toBe(TEST_DATA[index].result)
-  })
+  expect(1).toBe(1)
 })
